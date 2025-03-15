c0a6bb780048643d174152b91688fe965a4b36f9
awesomness560 <appsforyou6@gmail.com>
2025-03-15 14:40:49 -0500

    Finished end-screen

--- frontend/app/(game)/(play)/end-screen.tsx
+++ frontend/app/(game)/(play)/end-screen.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Button from '~/components/Button';
 import LeaderboardComponent, { Players } from '~/components/Leaderboard';
+import { useLobby } from '~/context/LobbyContext';
+import { router } from 'expo-router';
 
 const EndScreen = () => {
   //As of right now the avatar does not work
@@ -12,17 +14,32 @@ const EndScreen = () => {
     { avatar: '', plotPoints: 4 },
   ];
 
+  const { toggleVisible } = useLobby();
+
   return (
     <SafeAreaView className="flex-1 bg-background">
       <ScrollView className="mt-10" contentContainerStyle={{ flex: 1, alignItems: 'center' }}>
         <Text className="text-2xl font-bold text-backgroundText">
           And so the story comes to a close...
         </Text>
-        <Button title="View the Full Story" onPress={() => {}} className="mt-10 w-[80%]" />
+        <Button
+          title="View the Full Story"
+          onPress={() => {
+            toggleVisible();
+          }}
+          className="mt-10 w-[80%]"
+        />
         <Text className="mt-10 text-2xl font-bold text-backgroundText">
           Most Plot Points Chosen
         </Text>
         <LeaderboardComponent players={players} />
+        <Button
+          title="Home"
+          onPress={() => {
+            router.dismissTo('/(main)/home');
+          }}
+          className="mt-10 w-[80%]"
+        />
       </ScrollView>
     </SafeAreaView>
   );
