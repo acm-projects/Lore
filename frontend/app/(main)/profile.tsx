23b8740a90b4f32f76b92502dc36b2014d065559
YesMyNameIsMe <nguyen424787@gmail.com>
2025-04-09 19:52:32 -0500

    Guest WIP

--- frontend/app/(main)/profile.tsx
+++ frontend/app/(main)/profile.tsx
@@ -53,17 +53,12 @@ import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
     useFocusEffect(useCallback(() => { // Run these functions whenever profile page is loaded
       getCognitoSub()
-      getItems()
-      if(cognitoSub.length === 0) {
-        setGuest(true)
-      }
     }, []))
 
     useEffect(() => {
       if(cognitoSub.length != 0) {
         setGuest(false)
       }
-      getItems()
     }, [cognitoSub])
 
     const queryParams = {
@@ -73,6 +68,13 @@ import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
       ProjectionExpression: "Username, Email, Friends, ProfilePicURL, Stories, PlayerID, Biography",
       ExpressionAttributeValues: {
         ':subValue': cognitoSub
+      }
+    } 
+
+    const getGuestProfile = async (playerid: string) => {
+      const guestQueryParams = {
+        TableName: 'Players',
+        Key: 'PlayerID',
       }
     }
   
