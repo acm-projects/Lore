6f85fa731ab0e57a5ebafbdf96b8bba0f6919d56
YesMyNameIsMe <nguyen424787@gmail.com>
2025-04-22 10:39:04 -0500

    Bug Fix

--- frontend/app/(game)/(play)/score-page.tsx
+++ frontend/app/(game)/(play)/score-page.tsx
@@ -1,13 +1,9 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { SafeAreaView, Text, ActivityIndicator, Image, Dimensions, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, Text, ActivityIndicator } from 'react-native';
 import AnimatedScoreboard from '~/components/AnimatedScoreboard';
 import { useLobby } from '~/context/LobbyContext';
 import { socket } from '~/socket';
-import { useFocusEffect, useRouter } from 'expo-router';
-import { useAudio } from '~/context/AudioContext';
-import { Audio } from 'expo-av';
-import MuteButton from '~/components/MuteButton';
-import { useFonts } from 'expo-font';
+import { useRouter } from 'expo-router';
 
 type ScoreData = {
   id: string;
@@ -49,51 +45,20 @@ const ScorePage = () => {
     };
   }, [lobbyCode]);
 
-  const { playSound, stopSound, isMuted, toggleMute } = useAudio();
-  const soundRef = useRef<Audio.Sound | null>(null);
-  const scoreSFX = async () => {
-    const { sound } = await Audio. Sound.createAsync(
-      require('assets/score-sfx.mp3'),
-    );
-    soundRef.current = sound;
-    await sound.playAsync()
-  }
-  useFocusEffect( // For music, starts playing when writing screen is active, stops when navigated away
-    useCallback(() => {
-      if(!isMuted){
-        playSound(require('assets/score-track.mp3'));
-        scoreSFX()
-      } 
-      return() => {
-        stopSound();
-      }
-    }, [isMuted]
-  ))
-
   if (loading) {
     return (
       <SafeAreaView className="flex-1 bg-background items-center justify-center">
-        <Image source={require('assets/reading animation.gif')} className="h-32 w-32" />
+        <ActivityIndicator size="large" color="#fff" />
       </SafeAreaView>
     );
   }
 
-  useFonts({
-    JetBrainsMonoRegular: require('assets/fonts/JetBrainsMonoRegular.ttf'),
-    JetBrainsMonoBold: require('assets/fonts/JetBrainsMonoBold.ttf'),
-  });
-
   return (
     <SafeAreaView className="flex-1 bg-background">
-      <Image className="w-full" style={{ resizeMode: 'cover', position: 'absolute', height: Dimensions.get("window").height}} source={require("assets/bg9.gif")}/> 
-      <Text className="mt-5 text-center text-3xl font-bold text-backgroundText"
-            style={{fontFamily: 'JetBrainsMonoBold'}}>
+      <Text className="mt-5 text-center text-3xl font-bold text-backgroundText">
         Leaderboard
       </Text>
       <AnimatedScoreboard data={sampleData} />
-      <View className="w-full h-full justify-end items-end right-4 bottom-4" style={{position: 'absolute'}}>
-          <MuteButton/>
-      </View>
     </SafeAreaView>
   );
 };
